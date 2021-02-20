2c571780478e643b9f6e87697f2315ac84a458fc
Oleksandr Kovpashko <o.kovpashko@gmail.com>
2021-02-20 15:51:47 +0200

    chore: add npm scripts for building assets

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: 'resources/pages/index/index.js',
@@ -10,5 +13,6 @@ export default {
   plugins: [
     nodeResolve(),
     commonjs(),
+    isProduction && terser()
   ]
 };
